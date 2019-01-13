aac8b84f416ef3c3d922ecdd419c1f2e1b4ef409
Rajendra Kumar Padhy <rpadhy92@gmail.com>
2019-01-13 23:56:04 +0530

    Initial commit

---
+++ src/main/java/com/bnpparibas/atlentic/mailApplicatioin/App.java
@@ -0,0 +1,13 @@
+package com.bnpparibas.atlentic.mailApplicatioin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
